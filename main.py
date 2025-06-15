90f0d0130ea85c8956a0cba89c79a807bd7871f3
Brian <brianshrine48@outlook.com>
2025-06-15 11:21:18 -0600

    refactor: Structure code into main function

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello, FearlessInnovations!")
+def main():
+    """
+    This is the main function of the program.
+    """
+    print("Hello, FearlessInnovations! The journey has begun.")
+
+if __name__ == "__main__":
+    main()
